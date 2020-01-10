e46a0080b4f6a19ef83213595b520ce995b9a14c
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-10 16:38:33 -0700

    Redoing things so that I can do the advanced styling portion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import PictureComponent from "./components/PictureComponent";
+import logo from "./images/astronaut.png";
 import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <header className="App-header">
+        <img className="App-logo" src={logo} alt="astronaut" />
+        <div className="App Title">
+        <p>Photo of the Day from NASA</p>
+        </div>
+      </header>
+
+      <PictureComponent />
+
     </div>
   );
 }
